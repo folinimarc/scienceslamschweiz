c588657b7f71b5bc3e99b8d9d35ee3546cbca986
laiskasiili <marc.folini@gmail.com>
2024-02-20 17:45:23 +0000

    Remove overflow logic for now to improve mobile experience

--- static/js/main.js
+++ static/js/main.js
@@ -17,24 +17,8 @@ document.querySelectorAll('.img-overlay').forEach(el => {
     });
 });
 
-/*
-Align height of columns with height of images in corresponding rows.
-This is used to allow for y overflow of text elements when they exceed height of images in corresponding rows.
-*/
-function alignHeights() {
-    const elements = document.querySelectorAll('.align-col-height-with-row-img');
-    elements.forEach(el => {
-      const targetElement = el.parentNode.querySelector('img');
-      if (targetElement) {
-        el.style.maxHeight = `${targetElement.offsetHeight}px`;
-      }
-    });
-  }
-
-window.onload = () => {
-  alignHeights();
-  window.addEventListener('resize', alignHeights);
-  
+/* Run as soon as all content was loaded */
+window.onload = () => {  
   /* Enable all bootstrap 5 popover elements. */
   var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
   var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
